6289fb5dc67d031aa6022ea89872b1ccde046e15
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-06-27 09:18:03 +0200

    Implementation of owmonitoringreport class

--- classes/report/owmonitoringreport.php
+++ classes/report/owmonitoringreport.php
@@ -1,34 +1,61 @@
 <?php
 
-class OWMonitoringReport extends eZPersistentObject {
+class OWMonitoringReport {
 
-    protected $date;
     protected $identifier;
-    protected $status;
-    protected $values;
+    protected $reportData;
 
-    public function getValue( $name ) {
-
+    public function __contruct( $identifier ) {
+        $this->identifier = $identifier;
+        $this->reportData = array( );
     }
 
-    public function setValue( $name, $value ) {
-
+    public function hasData( $name ) {
+        return array_key_exists( $name, $this->reportData );
     }
 
-    public function appendValue( $name, $value ) {
-
+    public function getData( $name ) {
+        if( $this->hasData( $name ) ) {
+            return $this->reportData[$name];
+        }
+        return NULL;
     }
 
-    public function deleteValue( $name, $value ) {
-
+    public function setData( $name, $data ) {
+        if( !is_string( $name ) ) {
+            return FALSE;
+        }
+        $this->reportData[$name] = $data;
+        return TRUE;
     }
 
-    public function getValues( ) {
-
+    public function appendToData( $name, $data ) {
+        if( $this->hasData( $name ) ) {
+            $currentData = $this->getData( $name );
+            if( !is_array( $currentData ) ) {
+                $currentData = array( $currentData );
+            }
+            $currentData[] = $data;
+            $this->setData( $name, $currentData );
+        } else {
+            $this->setData( $name, $data );
+        }
     }
 
-    public function setValues( $values ) {
+    public function deleteData( $name ) {
+        if( $this->hasData( $name ) ) {
+            unset( $this->reportData[$name] );
+        }
+    }
 
+    public function getDatas( ) {
+        return $this->reportData;
+    }
+
+    public function setDatas( $dataArray ) {
+        foreach( $dataArray as $name => $data ) {
+            $this->setData( $name, $data );
+        }
     }
 
 }
