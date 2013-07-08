91063896bca380c326b8ff64d1b298657238815a
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-07-08 17:16:50 +0200

    Add method to make report specified in owmonitoring.ini

--- classes/owmonitoringreport.php
+++ classes/owmonitoringreport.php
@@ -7,7 +7,7 @@ class OWMonitoringReport {
 
     public function __construct( $identifier ) {
         if( empty( $identifier ) ) {
-            throw new Exception( "Report identifier must be set" );
+            throw new Exception( __METHOD__ . " : Report identifier must be set" );
         }
         $this->identifier = $identifier;
         $this->reportData = array( );
@@ -59,6 +59,10 @@ class OWMonitoringReport {
         return $this->reportData;
     }
 
+    public function countDatas( ) {
+        return count( $this->reportData );
+    }
+
     public function setDatas( $dataArray ) {
         foreach( $dataArray as $name => $data ) {
             $this->setData( $name, $data );
@@ -68,16 +72,60 @@ class OWMonitoringReport {
     public function sendReport( ) {
         $INI = eZINI::instance( 'owmonitoring.ini' );
         if( !$INI->hasVariable( 'OWMonitoring', 'MonitoringToolClass' ) ) {
-            OWMonitoringLogger::writeError( "[OWMonitoring]MonitoringToolClass not defined in owmonitoring.ini" );
+            OWMonitoringLogger::writeError( __METHOD__ . " : [OWMonitoring]MonitoringToolClass not defined in owmonitoring.ini" );
             return FALSE;
         }
         $monitoringToolClass = $INI->variable( 'OWMonitoring', 'MonitoringToolClass' );
         if( !class_exists( $monitoringToolClass ) ) {
-            OWMonitoringLogger::writeError( "Class $monitoringToolClass not found" );
+            OWMonitoringLogger::writeError( __METHOD__ . " : Class $monitoringToolClass not found" );
             return FALSE;
         }
         $tool = $monitoringToolClass::instance( );
         $tool->sendReport( $this );
+    }
+
+    static function makeReport( $reportName ) {
+        $INI = eZINI::instance( 'owmonitoring.ini' );
+        if( $INI->hasVariable( $reportName, 'Identifier' ) ) {
+            $identifier = $INI->variable( $reportName, 'Identifier' );
+        } else {
+            throw new Exception( __METHOD__ . " : [$reportName]Identifier not defined in owmonitoring.ini" );
+        }
+        if( $INI->hasVariable( $reportName, 'Tests' ) ) {
+            $testList = $INI->variable( $reportName, 'Tests' );
+        } else {
+            throw new Exception( __METHOD__ . " : [$reportName]Tests not defined in owmonitoring.ini" );
+        }
+        try {
+            $report = new OWMonitoringReport( $identifier );
+        } catch( Exception $e ) {
+            throw new Exception( __METHOD__ . " : Report instancation failed\n" . $e->getMessage( ) );
+        }
+        foreach( $testList as $testIdentifier => $testFunction ) {
+            list( $testClass, $testMethod ) = explode( '::', $testFunction );
+            $testClass = $reportName . '_' . $testClass;
+            $testMethod = 'test' . $testMethod;
+            $testFunction = $testClass . '::' . $testMethod;
+            if( !class_exists( $testClass ) ) {
+                OWMonitoringLogger::writeError( __METHOD__ . " : Class $testClass not found" );
+                continue;
+            } elseif( !is_callable( $testFunction ) ) {
+                OWMonitoringLogger::writeError( __METHOD__ . " : Can not call $testFunction method" );
+                continue;
+            } else {
+                try {
+                    $testValue = call_user_func( $testFunction );
+                } catch (  Exception $e ) {
+                    OWMonitoringLogger::writeError( __METHOD__ . " : " . $e->getMessage( ) );
+                    $testValue = FALSE;
+                }
+                $report->setData( $testIdentifier, $testValue );
+            }
+        }
+        if( $report->countDatas( ) == 0 ) {
+            throw new Exception( __METHOD__ . " : Report is empty" );
+        }
+        return $report;
     }
 
 }
