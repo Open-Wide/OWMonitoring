ce488bbe50b320ce3453afb7eb4ead1eabb38dcf
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-07-23 13:59:28 +0200

    Fix status value

--- classes/monitoring_tools/owmonitoringtool.php
+++ classes/monitoring_tools/owmonitoringtool.php
@@ -3,8 +3,8 @@
 abstract class OWMonitoringTool {
     
     const SENDING_SUCCESSFUL = 0;
-    const SENDING_INCOMPLETE = 0;
-    const SENDING_FAILED = 0;
+    const SENDING_INCOMPLETE = 1;
+    const SENDING_FAILED = 2;
 
     /* Send report to the monitoring tool */
     abstract public function sendReport( OWMonitoringReport $report );
